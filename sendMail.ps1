844ab8f5066c6ac0325fd6098a83e5e8e9557025
amitkr1729 <am_i_20@hotmail.com>
2019-01-03 15:46:04 +0530

    Update sendMail.ps1

--- sendMail.ps1
+++ sendMail.ps1
@@ -1,12 +1,12 @@
 ﻿#send mailer coder;;;;;
-$Username = "ak045346@cerner.net";
-$Password = "iCern@2k18";
+$Username = "username@domainname.net";
+$Password = "password";
 $path = ".\filename.txt";
 
 function Send-ToEmail([string]$email, [string]$attachmentpath){
 
     $message = new-object Net.Mail.MailMessage;
-    $message.From = "amit.kumar@cerner.com";
+    $message.From = "user.name@domain.com";
     $message.To.Add($email);
     $message.Subject = "subject textLINE here...";
     $message.Body = "body textLINE here...";
@@ -20,14 +20,14 @@ function Send-ToEmail([string]$email, [string]$attachmentpath){
     write-host "Mail Sent" ; 
     $attachment.Dispose();
  }
-Send-ToEmail  -email "amit.kumar@cerner.com" -attachmentpath $path;
+Send-ToEmail  -email "user.name@domain.com" -attachmentpath $path;
 
-<#
+<# multiline comment as raw template
 $SmtpServer = 'smtp.live.com'
-$SmtpUser = 'ak045346@cerner.net'
-$smtpPassword = 'iCern@2k18'
-$MailtTo = 'amit.kumar@cerner.com'
-$MailFrom = 'amit.kumar@cerner.com'
+$SmtpUser = ''
+$smtpPassword = ''
+$MailtTo = ''
+$MailFrom = ''
 $MailSubject = "Test using $SmtpServer" 
 $Credentials = New-Object System.Management.Automation.PSCredential -ArgumentList $SmtpUser, $($smtpPassword | ConvertTo-SecureString -AsPlainText -Force) 
 Send-MailMessage -To "$MailtTo" -from "$MailFrom" -Subject $MailSubject -SmtpServer $SmtpServer -UseSsl -Credential $Credentials
